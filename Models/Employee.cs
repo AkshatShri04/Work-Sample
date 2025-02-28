86d496cfaa7cd4b136a10e56f72e2d7069c3ba51
AkshatShri04 <akshatshri0407@gmail.com>
2025-02-28 11:20:02 -0500

    Add files via upload

---
+++ Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CrudApp.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public DateTime? HireDate { get; set; }
+    }
+}
